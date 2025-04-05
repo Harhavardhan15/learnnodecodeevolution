40ff5fe3018cc61401f9836abd51a45b6b3cf92d
unknown <hackinghandsome@gmail.com>
2025-04-06 00:07:17 +0530

    module cache

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const superHero=require('./superHero.js');
-require('./badman.js');
-require('./goodman.js');
-console.log(superHero)
+// const superHero=require('./superHero.js');
+// require('./badman.js');
+// require('./goodman.js');
+// console.log(superHero)
+const createHero=require('./createHero')
+console.log(createHero.getName())
+createHero.setName('venom')
+console.log(createHero.getName())
+const newhero=require('./createHero')
+console.log(newhero.getName())
