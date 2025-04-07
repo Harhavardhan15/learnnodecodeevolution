1b05f3a22e71d98656eae6287c6444952017ee05
unknown <hackinghandsome@gmail.com>
2025-04-07 11:42:11 +0530

    node path module

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-// const superHero=require('./superHero.js');
-// require('./badman.js');
-// require('./goodman.js');
-// console.log(superHero)
-const createHero=require('./createHero')
-const hero1=new createHero('hero1')
-console.log(hero1.getName())
+const path=require("path")
+// const createHero=require('./createHero')
+// const hero1=new createHero('hero1')
+// console.log(hero1.getName())
 
-hero1.setName('venom')
-console.log(hero1.getName())
-const newhero=new createHero('spidermane')
-console.log(newhero.getName())
+// hero1.setName('venom')
+// console.log(hero1.getName())
+// const newhero=new createHero('spidermane')
+// console.log(newhero.getName())
+
+// var obj1={
+//     name:'harsha'
+// }
+// let obj2=obj1;
+// obj2={
+//     name:'havarhanrsha'
+// }
+// console.log(obj1)
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname))
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
+// console.log(path.parse(__filename))
+// console.log(path.format(path.parse(__filename)))
+// console.log(path.isAbsolute(__filename));
+
+console.log(path.join('folder1','folder2'))
+console.log(path.join(__dirname,'iife.js'))
+
+console.log(path.resolve('folder','goodman.js'))
+
