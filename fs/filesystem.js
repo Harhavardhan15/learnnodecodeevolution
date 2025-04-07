a7fdd800fb855465b5ca88fd618d3119ca8f9e59
unknown <hackinghandsome@gmail.com>
2025-04-07 14:08:24 +0530

    sync file ready with blocking action

--- fs/filesystem.js
+++ fs/filesystem.js
@@ -1,2 +1,15 @@
 const fs = require('node:fs');
+console.log('one')
 console.log(fs.readFileSync('./example.txt','UTF-8'))
+console.log('two')
+
+fs.readFile('./example.txt','UTF-8',(error,data)=>{
+if(error){
+    console.log(error);
+}else{
+    console.log(data)
+}
+})
+console.log('three')
+
+
